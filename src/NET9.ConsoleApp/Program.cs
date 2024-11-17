3ea938557d0758b9a6c32fedd11bd83ed647a3a5
konnta0 <konnta0@gmail.com>
2024-11-18 00:28:51 +0900

    example

--- src/NET9.ConsoleApp/Program.cs
+++ src/NET9.ConsoleApp/Program.cs
@@ -1,3 +1,6 @@
 // See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// new escape sequence \e 
+Console.WriteLine("\e[41mHELLO, WORLD!");
+Console.WriteLine("\e[0mreset style");
+Console.WriteLine("\e[34m\e[4munderlined text");
