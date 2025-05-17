845d4afe329afde758925d03902e0c725bec1c94
konnta0 <konnta0@gmail.com>
2025-05-17 12:09:04 +0900

    kestrel settings

--- src/WebApplication1/Program.cs
+++ src/WebApplication1/Program.cs
@@ -1,9 +1,20 @@
+using System.Net;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddMagicOnion();
+builder.WebHost.UseKestrel(options =>
+{
+    options.Listen(IPAddress.Any, 7023, listenOptions =>
+    {
+        listenOptions.Protocols = HttpProtocols.Http2;
+        listenOptions.UseHttps();
+    });
+});
 
 var app = builder.Build();
 
